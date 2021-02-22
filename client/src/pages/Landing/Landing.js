f84bfbc0516ff0fa496b1e4e95d227abe6de40f5
ravisha7feb <ravisha7feb@gmail.com>
2021-02-22 22:03:45 +0530

    Navbar Added

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import "./style.css";
 import './Navbar.js';
-//import Navbar from './Navbar.js';
+import Navbar from './Navbar.js';
 
 function Landing() {
     return (
         <>
-                      
+           <Navbar></Navbar>   
     <header id="home" class="hero-area">   
     
       <div class="overlay">
         <span></span>
-        {/* <span></span> */}
       </div>
-      
+       
       <div class="container">     
-      {/* <Navbar></Navbar>  */}
+      
         <div class="row space-100">
           <div class="col-lg-6 col-md-12 col-xs-12">
             <div class="contents">
-              <h2 class="head-title">Annotation Mate</h2>
+              <h2 class="head-title">AnnoMate</h2>
               
               <h4><span
                 class="txt-rotate"
@@ -27,7 +26,7 @@ function Landing() {
                 data-rotate='[ "Your Data", "Annotated", "Right here!" ]'></span></h4>
               <div class="header-button">
                 <a href="#" class="btn btn-border-filled">Get Started</a>
-                <a href="contact" class="btn btn-border page-scroll">Contact Us</a>
+                <a href="contact" class="btn btn-border page-scroll">Register</a>
               </div>
             </div>
           </div>
--- client/src/pages/Landing/Navbar.js
+++ client/src/pages/Landing/Navbar.js
@@ -5,7 +5,36 @@ import "../../assets/LineIcons.css";
 
 function Navbar() {
     return (
-        <nav class="nav-menu">
+
+        <div class="nav">
+            <input type="checkbox" id="nav-check" />
+                <div class="nav-header">
+               
+                    <div class="nav-title">
+                    AnnoMate 
+                    </div>
+                
+                </div>
+            
+                <div class="nav-btn">
+                    <label for="nav-check">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                </div>
+
+                <div class="nav-links">
+                    <a href="#" target="_blank">Home</a>
+                    <a href="#" target="_blank">Tutorial</a>
+                    <a href="#" target="_blank">Features</a>
+                    <a href="#" target="_blank">Login</a>
+
+                </div>
+               
+        </div>
+    );
+        {/*<nav class="nav-menu">
             <ul>
                 <li><a href="#hero"><i class="lni lni-home"></i> <span>Home</span></a></li>
                 <li><a href="#dashboard"><i class="lni lni-dashboard"></i> <span>Dashboard</span></a></li>
@@ -14,9 +43,9 @@ function Navbar() {
 
             </ul>
 
-        </nav>
+    </nav>*/}
        
-    );
+    
 }
 
 export default Navbar;
