8ceb753ef005514226c9a3e1971e8b27ff919e35
unknown <nhi_maido@outlook.com>
2021-02-24 01:16:33 -0800

    add confirmation page for tasks

---
+++ client/src/components/CreateTaskForm/ConfirmationPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ConfirmationPage = () => {
+    return (
+      <div className="container">
+        <h1 id="confirmation" className="title">Task Successfully Created</h1>
+        <button id="confirm-button" type='button'>View My Tasks</button>
+      </div>
+    );
+  };
